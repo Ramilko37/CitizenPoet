94a1eb2b77a0de606184e7d14e9cb54b6191a169
Sergey Boronin <bo-ronin@yandex.ru>
2020-12-16 17:34:19 +0300

    feat: добавить третью и четвертую формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,60 @@
-const applyButton = document.querySelector(".content__button");
-const formButtonBack = document.querySelector("#form-info-back-button");
-const sideBlock = document.querySelector('.side-block');
 const infoSection = document.querySelector('.form-container-info');
+const ministrySection = document.querySelector('.form-container-ministry');
+const themeSection = document.querySelector('.form-container-theme');
+const confirmSection = document.querySelector('.form-container-confirm');
+
+const sideBlock = document.querySelector('.side-block');
+const applyButton = document.querySelector(".content__button");
+
+const formInfoButtonBack = infoSection.querySelector("#form-info-back-button");
+const formInfoButtonForvard = infoSection.querySelector("#form-info-forvard-button");
+
+
+const formMinistryButtonBack = ministrySection.querySelector("#form-ministry-back-button");
+const formMinistryButtonForvard = ministrySection.querySelector("#form-ministry-forvard-button");
+
+const formThemeButtonBack = themeSection.querySelector("#form-theme-back-button");
+const formThemeButtonForvard = themeSection.querySelector("#form-theme-forvard-button");
+
+const formConfirmButtonBack = confirmSection.querySelector("#form-confirm-back-button");
+const formConfirmButtonForvard = confirmSection.querySelector("#form-confirm-forvard-button");
 
 applyButton.addEventListener('click', () => {
     sideBlock.classList.add('side-block_visible');
     infoSection.classList.add('form-container_visible');
 })
 
-formButtonBack.addEventListener('click', () => {
+formInfoButtonBack.addEventListener('click', () => {
     sideBlock.classList.remove('side-block_visible');
     infoSection.classList.remove('form-container_visible');
+})
+
+formInfoButtonForvard.addEventListener('click', () => {
+  infoSection.classList.remove('form-container_visible');
+  ministrySection.classList.add('form-container_visible');
+})
+
+formMinistryButtonBack.addEventListener('click', () => {
+  ministrySection.classList.remove('form-container_visible');
+  infoSection.classList.add('form-container_visible');
+})
+
+formMinistryButtonForvard.addEventListener('click', () => {
+  ministrySection.classList.remove('form-container_visible');
+  themeSection.classList.add('form-container_visible');
+})
+
+formThemeButtonBack.addEventListener('click', () => {
+  themeSection.classList.remove('form-container_visible');
+  ministrySection.classList.add('form-container_visible');
+})
+
+formThemeButtonForvard.addEventListener('click', () => {
+  themeSection.classList.remove('form-container_visible');
+  confirmSection.classList.add('form-container_visible');
+})
+
+formConfirmButtonBack.addEventListener('click', () => {
+  themeSection.classList.add('form-container_visible');
+  confirmSection.classList.remove('form-container_visible');
 })
