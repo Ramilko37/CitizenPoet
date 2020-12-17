f186f02f21992204df53799a9d3dcc920d59958f
Sergey Boronin <bo-ronin@yandex.ru>
2020-12-17 20:01:24 +0300

    feat: верстка 4 и 5 форм

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
+const heroSection = document.querySelector('.content')
 const infoSection = document.querySelector('.form-container-info');
 const ministrySection = document.querySelector('.form-container-ministry');
 const themeSection = document.querySelector('.form-container-theme');
 const confirmSection = document.querySelector('.form-container-confirm');
+const sendSection = document.querySelector('.form-send');
 
 const sideBlock = document.querySelector('.side-block');
 const applyButton = document.querySelector(".content__button");
@@ -17,7 +19,9 @@ const formThemeButtonBack = themeSection.querySelector("#form-theme-back-button"
 const formThemeButtonForvard = themeSection.querySelector("#form-theme-forvard-button");
 
 const formConfirmButtonBack = confirmSection.querySelector("#form-confirm-back-button");
-const formConfirmButtonForvard = confirmSection.querySelector("#form-confirm-forvard-button");
+const formConfirmButtonSubmit = confirmSection.querySelector("#form-confirm-submit-button");
+
+const formSendButtonClose = sendSection.querySelector('#form-send-close-button');
 
 applyButton.addEventListener('click', () => {
     sideBlock.classList.add('side-block_visible');
@@ -57,4 +61,17 @@ formThemeButtonForvard.addEventListener('click', () => {
 formConfirmButtonBack.addEventListener('click', () => {
   themeSection.classList.add('form-container_visible');
   confirmSection.classList.remove('form-container_visible');
+})
+
+formConfirmButtonSubmit.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  confirmSection.classList.remove('form-container_visible');
+  sendSection.classList.add('form-container_visible');
+  heroSection.classList.add('hidden');
+  sideBlock.classList.remove('side-block_visible');
+})
+
+formSendButtonClose.addEventListener('click', () => {
+  heroSection.classList.remove('hidden');
+  sendSection.classList.remove('form-container_visible');
 })
